1b4cc0c1bf59f7d19dbcd63b51b790132d1c843f
mjsp <miguel.s.prata@tecnico.pt>
2018-08-17 12:00:09 +0100

    Committed missing files with libraries and data loader.

---
+++ codingChallenges/codingChallenges/libraries.cpp
@@ -0,0 +1,9 @@
+//
+//  auxiliarFunctions.cpp
+//  CodingChallenges
+//
+//  Created by Miguel Prata on 03/06/2018.
+//  Copyright © 2018 Miguel Prata. All rights reserved.
+//
+
+#include "libraries.hpp"
