62e216fa2c65f494556de24909c9444f2aeb57d6
mjsp <miguel.s.prata@tecnico.pt>
2018-08-17 12:17:35 +0100

    File renaming 1/1

--- codingChallenges/codingChallenges/libraries.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  auxiliarFunctions.cpp
-//  CodingChallenges
-//
-//  Created by Miguel Prata on 03/06/2018.
-//  Copyright © 2018 Miguel Prata. All rights reserved.
-//
-
-#include "libraries.hpp"
