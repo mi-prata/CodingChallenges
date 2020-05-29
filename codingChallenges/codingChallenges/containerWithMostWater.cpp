b54dc3ccf101bffc8bebdf12e4ae9ec19ccd5adf
mjsp <miguel.s.prata@tecnico.pt>
2020-05-29 12:40:32 +0200

    New coding challenge: Container With Most Water.

---
+++ codingChallenges/codingChallenges/containerWithMostWater.cpp
@@ -0,0 +1,35 @@
+//
+//  containerWithMostWater.cpp
+//  codingChallenges
+//
+//  Created by Miguel Prata on 29/05/2020.
+//  Copyright © 2020 mjsp. All rights reserved.
+//
+#include "containerWithMostWater.hpp"
+
+
+int maxArea(std::vector<int>& height) {
+    //height to Indexes
+    
+    int finalResult(0);
+    //two pointers
+    const auto elements = height.size();
+    int i = 0;
+    int j = elements - 1;
+    while (i < elements and
+           j > 0 and
+           j > i)
+    {
+        const auto leftHeight = height[i];
+        const auto rightHeight = height[j];
+        const auto area = fmin(leftHeight, rightHeight) * abs(j - i);
+        
+        if (area > finalResult) finalResult = area;
+        if (leftHeight < rightHeight) i++;
+        else j--;
+    }
+    return finalResult;
+}
+
+
+
---
+++ codingChallenges/codingChallenges/containerWithMostWater.hpp
@@ -0,0 +1,16 @@
+//
+//  containerWithMostWater.hpp
+//  codingChallenges
+//
+//  Created by Miguel Prata on 29/05/2020.
+//  Copyright © 2020 mjsp. All rights reserved.
+//
+
+#ifndef containerWithMostWater
+#define containerWithMostWater
+
+#include "libraries.hpp"
+
+int maxArea(std::vector<int>& height);
+
+#endif /* containerWithMostWater_hpp */
